7474f5e8e301cdabe84984f32c32c24d71b4d8cc
Alaa saeed <113705228+Alaa188@users.noreply.github.com>
2024-07-28 11:37:53 +0300

    add js src

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let zero=0;
+let counter=3;
+let arr=['kiro','eslam','sayed','tarek','ali','marwan'];
+console.log((arr.slice(zero,++counter)).reverse());
+
+console.log((arr.slice(1,3)).reverse());
+
+console.log((arr[2][0]+arr[3][0]+arr[3][1]+arr[3][2]).toUpperCase())
